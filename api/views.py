c3d35146eef62a9933d0111d89b201de456f9118
Aman Rawat <kirmolaa@gmail.com>
2024-05-09 06:52:00 +0530

    fine grained http methods

--- api/views.py
+++ api/views.py
@@ -11,15 +11,18 @@ from django.shortcuts import get_list_or_404
 class UserViewSet(ModelViewSet):
     queryset = ExtendedUser.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ["get", "post"]
 
 
 class PostsViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    http_method_names = ["get", "post", "put", "patch"]
 
 
 class SpecificPostsViewSet(ModelViewSet):
     serializer_class = PostSerializer
+    http_method_names = ["get"]
 
     def get_queryset(self):
         user_pk = self.kwargs.get("user_pk")
