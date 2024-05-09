3a7c3ee8e14048d8ff21e7ffadbfe7eed1075b43
Aman Rawat <kirmolaa@gmail.com>
2024-05-09 07:30:41 +0530

    removed unused methods and http methods

--- api/views.py
+++ api/views.py
@@ -12,26 +12,13 @@ from django.shortcuts import get_list_or_404
 class UserViewSet(ModelViewSet):
     queryset = ExtendedUser.objects.all()
     serializer_class = UserSerializer
-    http_method_names = ["get", "post"]
 
 
 class PostsViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    http_method_names = ["get", "post", "put", "patch", "delete"]
-
-
-class SpecificPostsViewSet(ModelViewSet):
-    serializer_class = PostSerializer
-    http_method_names = ["get"]
-
-    def get_queryset(self):
-        user_pk = self.kwargs.get("user_pk")
-        post_id = self.kwargs.get("pk")
-        return get_list_or_404(Post, post_author=user_pk)
 
 
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    http_method_names = ["get", "post", "put", "patch", "delete"]
