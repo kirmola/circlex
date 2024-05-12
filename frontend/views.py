31bf1921349d51ad8191b896d9d74cfe62fd1482
Aman Rawat <kirmolaa@gmail.com>
2024-05-12 12:57:30 +0530

    added posts to user page

--- frontend/views.py
+++ frontend/views.py
@@ -4,6 +4,7 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import DetailView
 from api.models import *
+from django.core.paginator import Paginator, Page, EmptyPage
 
 
 class UserView(DetailView):
@@ -13,7 +14,13 @@ class UserView(DetailView):
     slug_url_kwarg = "username"
     
     def get_queryset(self):
-        return super().get_queryset().only("username", "first_name", "last_name", "date_joined", "bio", "profile_picture")    
+        return super().get_queryset().values("username", "first_name", "last_name", "date_joined", "bio", "profile_picture")    
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["all_posts"] = Post.objects.filter(post_author__username=self.kwargs.get("username")).all().order_by("-created_on")
+        return context
+    
     
 class PostView(DetailView):
     template_name = "posts.html"
